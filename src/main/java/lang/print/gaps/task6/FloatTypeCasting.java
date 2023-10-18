3ee280a648e1c23252441eb1a3ff32cbee1e5249
Fanny Dorthey <fannydor@ev-k6.kzn.21-school.ru>
2023-10-18 20:25:37 +0300

    agw5gerg

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,6 +2,7 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
-        System.out.println(Math.ceil(numberToBeRounded));
+        int roundedNumber = Math.round(numberToBeRounded);
+        System.out.println(roundedNumber);
     }
 }
